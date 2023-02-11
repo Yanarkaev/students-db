f997e146248602dc333c6b95cdc5ff62b72c7f65
Yanarkaev <hasssanbek@gmail.com>
2023-02-11 19:02:43 +0300

    Компонент инпута

--- src/components/iu/Input/Input.jsx
+++ src/components/iu/Input/Input.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import styles from "./Input.module.scss"
+import React, { useState } from "react";
+import styles from "./Input.module.scss";
 
-export const Input = () => {
-    return (
-        <div>
-            Input
-        </div>
-    );
+export const Input = ({
+  type,
+  value,
+  placeholder = "",
+  variant = "standard",
+  className,
+  onChange,
+}) => {
+  const [focus, setFocus] = useState(false);
+  return (
+    <div className={styles.InputWrapper}>
+      <input
+        type={type}
+        className={`${styles.Input} ${styles[variant]} ${className}`}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        value={value}
+        onChange={onChange}
+      />
+      <span
+        className={`${styles.placeholder} ${
+          focus || value ? styles.focused : ""
+        }`}
+      >
+        {placeholder}
+      </span>
+    </div>
+  );
 };
