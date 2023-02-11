fdcc51ec652989175b1c5300494297496f3bb6e0
Yanarkaev <hasssanbek@gmail.com>
2023-02-11 23:26:58 +0300

    Компонент авторизации

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./components";
 import { Input } from "./components/iu";
+import Auth from "./features/auth-page/Auth";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Input placeholder="standard" variant="standard" />
+      {/* <Header />
+      <Input placeholder="standard" variant="standard" /> */}
+      <Auth />
     </div>
   );
 }
--- src/features/auth-page/Auth.jsx
+++ src/features/auth-page/Auth.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button, Input } from "../../components/iu";
+import styles from "./Auth.module.scss";
 
 const Auth = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [data, setData] = useState({
+    login: "",
+    password: "",
+  });
+
+  const handleData = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+    console.log(e.target.value);
+  };
+
+  const handleAuth = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className={styles.Auth}>
+      <h1>Вход</h1>
+
+      <form onSubmit={handleAuth}>
+        <Input
+          name="login"
+          value={data.login}
+          placeholder="Логин"
+          onChange={handleData}
+          variant="outlined"
+          type="email"
+          className={styles.input}
+        />
+        <Input
+          name="password"
+          value={data.password}
+          placeholder="Пароль"
+          onChange={handleData}
+          variant="outlined"
+          type="password"
+          className={styles.input}
+        />
+
+        <Button variant="enter" className={styles.button}>Войти</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
